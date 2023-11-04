cca937fe28d59c0c759c2474974e626db0aeed1d
dev-shenlong <dev.shenlong@gmail.com>
2023-11-04 22:10:50 +0530

    added login page

--- website/app.py
+++ website/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Worldd"
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
